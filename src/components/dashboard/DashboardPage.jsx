487d5d97eff4d4dd413a9dcad2c46852c1d74040
Sid Saythongphet <sid.saythongphet@gmail.com>
2022-04-03 10:28:40 -0400

    format user dashboard

---
+++ src/components/dashboard/DashboardPage.jsx
@@ -0,0 +1,62 @@
+import { Container, Grid, Typography } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { baseURL } from '../../Globals';
+import StyledBox from '../styles/StyledBox';
+import FollowingDisplay from './FollowingDisplay';
+import UserInfoContainer from './UserInfoContainer';
+import UsersPosts from './UsersPosts';
+
+const DashboardPage = ({ currentUser, onUpdateUser, followees, notifyErrors }) => {
+  const [usersPosts, setUsersPosts] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(baseURL + `/users/${currentUser.id}/posts`, {
+        method: "GET",
+        headers: {
+          "Authorization": `Bearer ${ localStorage.getItem('jwt') }`
+        }
+      })
+      const data = await response.json()
+      if (response.ok) {
+        setUsersPosts(data)
+      } else {
+        console.log(data.error)
+      }
+    }
+    fetchData()
+  }, [])
+
+  const handleDeletePost = (deletedPost) => {
+    const updatePosts = usersPosts.filter(post => post.id !== deletedPost.id)
+    setUsersPosts(updatePosts)
+  }
+  
+  const renderUsersPosts = usersPosts.map(post => {
+    return (
+      <Grid item key={ post.id }>
+        <UsersPosts post={ post } currentUser={ currentUser } onDeletePost={ handleDeletePost }/>
+      </Grid>  
+    )
+  })
+
+  return (
+    <Container>
+      <Grid container spacing={2}>
+        <Grid item xs={12} container justifyContent="center">
+          <UserInfoContainer currentUser={ currentUser } onUpdateUser={ onUpdateUser } notifyErrors={ notifyErrors }/>
+        </Grid>
+        <Grid item xs={12} container justifyContent="center">
+          <FollowingDisplay currentUser={ currentUser } followees={ followees }/>
+        </Grid>
+        <Grid item xs={12} container justifyContent="center">
+          <StyledBox width="900px">
+            { usersPosts > 0 ? renderUsersPosts : <Typography textAlign='center'>Create your first post</Typography> }
+          </StyledBox>
+        </Grid>
+      </Grid>
+    </Container>
+  )
+}
+
+export default DashboardPage
--- src/components/dashboard/UserInfoContainer.jsx
+++ src/components/dashboard/UserInfoContainer.jsx
@@ -1,39 +1,133 @@
-import { Avatar, Box, Grid, Typography } from '@mui/material';
 import React, { useState } from 'react';
+import { baseURL, headers } from '../../Globals';
+import { Avatar, Button, Grid, Stack, TextField, Typography } from '@mui/material';
 import StyledBox from '../styles/StyledBox';
 import UpdateProfilePicture from './UpdateProfilePicture';
 
 
-const UserInfoContainer = ({ currentUser, onUpdateUser }) => {
+const UserInfoContainer = ({ currentUser, onUpdateUser, notifyErrors }) => {
   const { id, first_name, last_name, username, email, profile_picture_url} = currentUser
+  const [editable, setEditable] = useState(false)
+  const [updateForm, setUpdateForm] = useState({
+    first_name: first_name,
+    last_name: last_name,
+    username: username,
+    email: email
+  })
+
+  const handleChange = (e) => {
+    setUpdateForm({
+      ...updateForm,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const response = await fetch(baseURL + `/users/${id}`, {
+      method: "PATCH",
+      headers: {
+        ...headers,
+        "Authorization": `Bearer ${ localStorage.getItem('jwt') }`
+      },
+      body: JSON.stringify(updateForm)
+    })
+    const data = await response.json()
+    if (response.ok) {
+      console.log(data)
+    } else {
+      data.errors.map(err => notifyErrors(err))
+    }
+  }
 
   return (
-    <Grid item>
-      <StyledBox>
-        <Grid container>
-          <Grid item>
+    <StyledBox width="900px">
+      <Grid container sx={{ height: "100%" }} justifyContent="space-evenly">
+        <Grid item xs={5} container justifyContent="center">
+          <Stack alignItems="center" spacing={2}>
             <Avatar alt={ `${first_name} ${last_name}` } src={ profile_picture_url === "" ? "/static/images/avatar/2.jpg" : profile_picture_url } sx={{ width: 200, height: 200 }}/>
-            <UpdateProfilePicture id={ id } onUpdateUser={ onUpdateUser } />
-          </Grid>
+            <UpdateProfilePicture id={ id } onUpdateUser={ onUpdateUser } notifyErrors={ notifyErrors }/>
+          </Stack>
+        </Grid>
+        <Grid item xs={7} container>
           <Grid item xs={12}>
-            <Typography>First Name</Typography>
-            <Typography>{ first_name }</Typography>
+            <Typography variant="h5">General Account Settings</Typography>
           </Grid>
-          <Grid item xs={12}>
-            <Typography>Last Name</Typography>
-            <Typography>{ last_name }</Typography>
+          <Grid item xs={12} container>
+            <Grid item xs={3}>
+              <Typography>Name</Typography>
+            </Grid>
+            <Grid item xs={9} container spacing={1}>
+              <Grid item xs={6}>
+                <TextField 
+                  id="outlined-basic" 
+                  disabled={ !editable ? true : null }
+                  fullWidth 
+                  label="First Name"
+                  variant="outlined" 
+                  size="small" 
+                  name="first_name" 
+                  value={ updateForm.first_name } 
+                  onChange={ handleChange }
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <TextField 
+                  id="outlined-basic" 
+                  disabled={ !editable ? true : null }
+                  fullWidth 
+                  label="Last Name" 
+                  variant="outlined" 
+                  size="small" 
+                  name="last_name" 
+                  value={ updateForm.last_name } 
+                  onChange={ handleChange }
+                />
+              </Grid>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <Typography>Username</Typography>
-            <Typography>{ username }</Typography>
+          <Grid item xs={12} container>
+            <Grid item xs={3}>
+              <Typography>Username</Typography>
+            </Grid>
+            <Grid item xs={9}>
+              <TextField 
+                id="outlined-basic"
+                disabled={ !editable ? true : null }
+                fullWidth 
+                variant="outlined"
+                size="small" 
+                name="username" 
+                value={ updateForm.username } 
+                onChange={ handleChange }
+              />
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <Typography>Email</Typography>
-            <Typography>{ email }</Typography>
+          <Grid item xs={12} container>
+            <Grid item xs={3}>
+              <Typography>Email</Typography>
+            </Grid>
+            <Grid item xs={9}>
+              <TextField 
+                id="outlined-basic"
+                disabled={ !editable ? true : null }
+                fullWidth 
+                variant="outlined"
+                size="small" 
+                name="email" 
+                value={ updateForm.email } 
+                onChange={ handleChange }
+              />
+            </Grid>
           </Grid>
         </Grid>
-      </StyledBox>
-    </Grid>
+        <Grid item justifySelf="end">
+          <Button onClick={ (e) => setEditable(!editable) }>{ !editable ? "Edit" : "Cancel" }</Button>
+          {editable ? <Button onClick={ handleSubmit }>Submit</Button> : null }
+        </Grid>
+      </Grid>
+    </StyledBox>
   )
 }
 
